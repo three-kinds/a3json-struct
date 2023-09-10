e9884671a383cfd674db8fe746cfd6e0f9651b90
三样 <3179158552@qq.com>
2023-09-10 19:52:25 +0800

    Finish DateField and DateTimeField unit tests

---
+++ tests/test_fields/test_date_field.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from a3json_struct import struct, errors
+
+
+class T(unittest.TestCase):
+
+    def test__cast_to_python(self):
+        class User(struct.JsonStruct):
+            birthday = struct.DateField()
+
+        user = User()
+        user.birthday = '20a2-10-01'
+        with self.assertRaises(errors.ValidationError):
+            user.full_clean()
+
+        user.birthday = '2020-01-10'
+        user.full_clean()
