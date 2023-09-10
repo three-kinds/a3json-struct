3008b45d674f88502218f8ab8090052779c255a1
三样 <3179158552@qq.com>
2023-09-10 16:57:25 +0800

    Finish errors unit tests

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -5,6 +5,19 @@ from a3json_struct import errors
 
 class T(unittest.TestCase):
 
-    def test__repr(self):
-        e = errors.ValidationError("")
-        self.assertIn(errors.ValidationError.__name__, repr(e))
+    def test__str(self):
+        message = "message"
+        e = errors.ValidationError(message)
+        self.assertEqual(str(e), message)
+        # set field
+        field = "field"
+        e.set_field(field)
+        self.assertEqual(str(e), f"{field}: {message}")
+        # set index
+        e = errors.ValidationError(message)
+        index0 = 0
+        index1 = 1
+        e.set_index(index0)
+        e.set_index(index1)
+        e.set_field(field)
+        self.assertEqual(str(e), f"{field}[{index1}][{index0}]: {message}")
