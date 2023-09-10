516d107f628c15a170845b04e5d0b05eaca50ff5
三样 <3179158552@qq.com>
2023-09-10 14:39:38 +0800

    Finish FloatField

---
+++ a3json_struct/fields/float_field.py
@@ -0,0 +1,19 @@
+from typing import Any
+
+from a3json_struct.errors import ValidationError
+from .abstract_field import AbstractField
+
+
+class FloatField(AbstractField):
+
+    def _cast_to_python(self, value: Any) -> float:
+        if isinstance(value, float):
+            return value
+
+        try:
+            return float(str(value))
+        except (TypeError, ValueError):
+            raise ValidationError(f'Value "{value}" must be a float.')
+
+    def to_json(self, cleaned_value: float) -> float:
+        return cleaned_value
