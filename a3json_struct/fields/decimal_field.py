229f1b8b422831b7f8ada5d53cc55813a9ec48ba
三样 <3179158552@qq.com>
2023-09-10 14:37:35 +0800

    Finish DecimalField

---
+++ a3json_struct/fields/decimal_field.py
@@ -0,0 +1,20 @@
+from typing import Any
+from decimal import Decimal, DecimalException
+
+from a3json_struct.errors import ValidationError
+from .abstract_field import AbstractField
+
+
+class DecimalField(AbstractField):
+
+    def _cast_to_python(self, value: Any) -> Decimal:
+        if isinstance(value, Decimal):
+            return value
+
+        try:
+            return Decimal(str(value))
+        except DecimalException:
+            raise ValidationError(f'Value "{value}" must be a decimal number.')
+
+    def to_json(self, cleaned_value: Decimal) -> str:
+        return str(cleaned_value)
