8a40b77ae843197c41c0ec7c245cd97df351a86a
三样 <3179158552@qq.com>
2023-09-10 12:22:20 +0800

    Finish BooleanField

---
+++ a3json_struct/fields/boolean_field.py
@@ -0,0 +1,24 @@
+from typing import Any
+
+from a3json_struct.errors import ValidationError
+from .abstract_field import AbstractField
+
+
+class BooleanField(AbstractField):
+
+    def _cast_to_python(self, value: Any) -> bool:
+        if isinstance(value, bool):
+            return value
+
+        value = str(value).lower()
+
+        if value in ('t', 'true', '1'):
+            return True
+
+        if value in ('f', 'false', '0'):
+            return False
+
+        raise ValidationError(f'Value "{value}" must be either True or False.')
+
+    def to_json(self, cleaned_value: bool) -> bool:
+        return cleaned_value
