774f7a9fcee4e1f9d8d967331576b36d35f8e548
三样 <3179158552@qq.com>
2023-09-10 19:49:29 +0800

    Finish IntegerField unit tests

---
+++ tests/test_fields/test_integer_field.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from a3json_struct import struct, errors
+
+
+class T(unittest.TestCase):
+
+    def test__validators(self):
+        class User(struct.JsonStruct):
+            age = struct.IntegerField(min_value=0, max_value=150)
+
+        user = User()
+        user.age = -1
+        with self.assertRaises(errors.ValidationError):
+            user.full_clean()
+
+        user.age = 250
+        with self.assertRaises(errors.ValidationError):
+            user.full_clean()
+
+        user.age = 32
+        user.full_clean()
+
+        # invalid
+        user.age = 'abc'
+        with self.assertRaises(errors.ValidationError):
+            user.full_clean()
+
+    def test__choices(self):
+        class User(struct.JsonStruct):
+            grade = struct.IntegerField(choices={1, 2, 3, 4, 5, 6})
+
+        user = User()
+        user.grade = 100
+        with self.assertRaises(errors.ValidationError):
+            user.full_clean()
+
+        user.grade = 3
+        user.full_clean()
