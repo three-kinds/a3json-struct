574419f84e41bae98720f4883897361c035117a5
三样 <3179158552@qq.com>
2023-09-10 19:45:51 +0800

    Finish FloatField unit tests

--- a3json_struct/fields/float_field.py
+++ a3json_struct/fields/float_field.py
@@ -1,10 +1,27 @@
 from typing import Any
 
 from a3json_struct.errors import ValidationError
+from a3json_struct import validators
 from .abstract_field import AbstractField
 
 
 class FloatField(AbstractField):
+    
+    def __init__(
+            self,
+            min_value: float = None,
+            max_value: float = None,
+            *args, **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+
+        self._min_value = min_value
+        self._max_value = max_value
+
+        if self._min_value is not None:
+            self._validators.append(validators.MinValueValidator(self._min_value))
+        if self._max_value is not None:
+            self._validators.append(validators.MaxValueValidator(self._max_value))
 
     def _cast_to_python(self, value: Any) -> float:
         if isinstance(value, float):
---
+++ tests/test_fields/test_float_field.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from a3json_struct import struct, errors
+
+
+class T(unittest.TestCase):
+
+    def test__validators(self):
+        class User(struct.JsonStruct):
+            height_m = struct.FloatField(min_value=0.0, max_value=2.5)
+
+        user = User()
+        user.height_m = -1
+        with self.assertRaises(errors.ValidationError):
+            user.full_clean()
+
+        user.height_m = 3
+        with self.assertRaises(errors.ValidationError):
+            user.full_clean()
+
+        user.height_m = 1.8
+        user.full_clean()
+
+        # invalid
+        user.height_m = 'abc'
+        with self.assertRaises(errors.ValidationError):
+            user.full_clean()
